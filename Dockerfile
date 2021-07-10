de06c26b903ef8ef109fec9fddb6270f28b4d2cb
am3228 <am3228@njit.edu>
2021-07-10 14:40:02 -0400

    Feature: Refactor for travis testing and running multiple tests

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM python:3
+FROM python:3.7
 
-ADD src /src
+ADD . .
 
-CMD [ "python", "./src/CalculatorTests.py" ]
+Run pip install --upgrade pip
+
+CMD [ "python", "-m", "unitest", "discover", "-s", "Tests"]
--- src/CsvReader.py
+++ src/CsvReader.py
@@ -1,17 +1,17 @@
 import csv
-from pprint import pprint
+from pathlib import Path
 
 def ClassFactory(class_name, dictionary):
     return type(class_name, (object,), dictionary)
-
 
 class CsvReader:
     data = []
 
     def __init__(self, filepath):
         self.data = []
-
-        with open(filepath) as text_data:
+        relative = Path(filepath)
+        absolute = relative.absolute()
+        with open(absolute) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
