398111c04874682236ac312dc8a1a126098c3231
am3228 <am3228@njit.edu>
2021-07-12 19:28:50 -0400

    Task: Variance

---
+++ src/Statistics/Variance.py
@@ -0,0 +1,16 @@
+from Statistics.Mean import mean
+from Calculator.Square import square
+from Calculator.Division import division
+
+def variance(data):
+    try:
+        meanData = mean(data)
+        totalVals = len(data)
+        var = 0
+        for a in data:
+            var = var + square(a = meanData)
+        result = division(var, totalVals)
+
+        return result
+    except ValueError:
+        print("Error: That is an empty array. Please try again")
