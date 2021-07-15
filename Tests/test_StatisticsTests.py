50561e243b8a2e8c0e8f56c8faa5a81c64f85e5e
am3228 <am3228@njit.edu>
2021-07-14 23:14:24 -0400

    Fix: trying to get tests to run

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -11,34 +11,34 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_statistics(self):
         self.assertIsInstance(self.statistics, Statistics)
 
-    def test_mean(self):
+    def mean(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Mean.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
-        self.assertEqual(self.statistics.statistics_mean(value_data), 22.67)
+        self.assertEqual(self.statistics.get_mean(value_data), 22.67)
         self.assertEqual(self.statistics.result, 22.67)
 
-    def test_median(self):
+    def median(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Median.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
-        self.assertEqual(self.statistics.statistics_median(value_data), 22)
+        self.assertEqual(self.statistics.get_median(value_data), 22)
         self.assertEqual(self.statistics.result, 22)
 
-    def test_mode(self):
+    def mode(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Mode.csv').data
-        value_data = [int(row["Value 1"]) for row in test_data]
-        self.assertEqual(self.statistics.statistics_mode(value_data), [32])
+        value_data = [int(row['Value 1']) for row in test_data]
+        self.assertEqual(self.statistics.get_mode(value_data), [32])
         self.assertEqual(self.statistics.result, [32])
 
-    def test_variance(self):
+    def variance(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Variance.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
-        self.assertEqual(self.statistics.statistics_variance(value_data), np.var(value_data))
+        self.assertEqual(self.statistics.get_variance(value_data), np.var(value_data))
         self.assertEqual(self.statistics.result, np.var(value_data))
 
-    def test_standard_deviation(self):
+    def standard_deviation(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Standard Deviation.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
-        self.assertEqual(self.statistics.Standard_Deviation(value_data), np.std(value_data), 3)
+        self.assertEqual(self.statistics.get_standard_deviation(value_data), np.std(value_data), 3)
         self.assertEqual(self.statistics.result, np.std(value_data))
 
 if __name__ == '__main__':
