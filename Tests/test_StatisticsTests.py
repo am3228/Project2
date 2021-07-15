6b59eab04e0e89c16ca5401007f9a6e22ad3eefe
am3228 <am3228@njit.edu>
2021-07-14 23:33:59 -0400

    Fix: trying to get tests to run

--- Tests/test_StatisticsTests.py
+++ Tests/test_StatisticsTests.py
@@ -11,31 +11,31 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_statistics(self):
         self.assertIsInstance(self.statistics, Statistics)
 
-    def mean(self):
+    def test_mean(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Mean.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
         self.assertEqual(self.statistics.get_mean(value_data), 22.67)
         self.assertEqual(self.statistics.result, 22.67)
 
-    def median(self):
+    def test_median(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Median.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
         self.assertEqual(self.statistics.get_median(value_data), 22)
         self.assertEqual(self.statistics.result, 22)
 
-    def mode(self):
+    def test_mode(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Mode.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
         self.assertEqual(self.statistics.get_mode(value_data), [32])
         self.assertEqual(self.statistics.result, [32])
 
-    def variance(self):
+    def test_variance(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Variance.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
         self.assertEqual(self.statistics.get_variance(value_data), np.var(value_data))
         self.assertEqual(self.statistics.result, np.var(value_data))
 
-    def standard_deviation(self):
+    def test_standard_deviation(self):
         test_data = Csv_Reader('Tests/Data/Unit Test Standard Deviation.csv').data
         value_data = [int(row['Value 1']) for row in test_data]
         self.assertEqual(self.statistics.get_standard_deviation(value_data), np.std(value_data), 3)
